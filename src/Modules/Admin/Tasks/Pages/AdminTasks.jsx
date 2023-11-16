9b5e9886d7328a219d9164ba410edb9b9057a219
sarameree <sarameree52@gmail.com>
2023-11-15 20:45:48 -0800

    sar

--- src/Modules/Admin/Tasks/Pages/AdminTasks.jsx
+++ src/Modules/Admin/Tasks/Pages/AdminTasks.jsx
@@ -1,10 +1,13 @@
 import './AdminTasks.css'
 import imgTeam from './../../../../assets/imgTeam.png';
 import { FaLongArrowAltRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function AdminTasks() {
     return (
         <div>
+            <Link to={'/dashboard/tasks/subtask'}><button>go toSubTask</button></Link>
+            <Link to={'/dashboard/tasks/assigntask'}><button>go AssignTask</button></Link>
             <div className='sa_divTitle'>
                 <div>
                     <p className="header-style">Projects / Addodle</p>
