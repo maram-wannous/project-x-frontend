8485445d8493c6f24f8420b8721a657e6ddd62ce
sarameree <sarameree52@gmail.com>
2023-12-09 16:20:06 -0800

    P

--- src/Modules/Admin/AdminProjects/Pages/AdminAllProjects.jsx
+++ src/Modules/Admin/AdminProjects/Pages/AdminAllProjects.jsx
@@ -4,12 +4,30 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { Link } from 'react-router-dom'
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const AdminAllProjects = () => {
-
   const isAdmin = true;
 
+  const token = localStorage.getItem('bearer');
+  const [Projects, setProjects] = useState([]);
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/projects?page=1&pageSize=6', {
+      headers: {
+        Accept: 'application/json',
+        AUTHORIZATION: `Bearer ${token}`,
+      },
+    })
+    .then((data)=> setProjects(data.data.projects.data))
+    .catch((err)=> console.log(err));
+  }, []);
+
+
+  state = {
+    activeSlide: 0,
+    activeSlide2: 0
+  };
   var settings = {
     dots: true,
     customPaging: (i) => {
@@ -19,6 +37,8 @@ export const AdminAllProjects = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    beforeChange: (current, next) => this.setState({activeSlide: next }),
+    aftereChange: current => this.setState({activeSlide2: current }),
     initialSlide: 0,
     responsive: [
       {
@@ -56,35 +76,36 @@ export const AdminAllProjects = () => {
       </div>
 
       <Slider {...settings} className='sa_containerSlideAllProjects'>
-      <div className='sa_AdminAllProjects_BodyDiv'>
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>          
+        <div className='sa_AdminAllProjects_BodyDiv'>
+          {Projects.map((item) => (
+            <div key={item.id}>
+              <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} title={item.title} status={item.status} /></Link>
+            </div>
+          ))}
         </div>
         <div className='sa_AdminAllProjects_BodyDiv'>
-        <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-        <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-        <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-        <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-        <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-        <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-        <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-        <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} title="sara" status="OnTrack" description="sssssssssssssssssssssjjjjjjjjj''''''''''''''llllllllllpppppppppsssssssssssssssssssssjjjjjjjjj''''''''''''''llllllllllppppppppp"/></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
         </div>
         <div className='sa_AdminAllProjects_BodyDiv'>
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
-          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link> 
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+        </div>
+        <div className='sa_AdminAllProjects_BodyDiv'>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
+          <Link to={'/dashboard/adminprojects/details/1'}><CardProject isAdmin={isAdmin} /></Link>
         </div>
       </Slider>
     </div>
